53d125410855390465b8c450b8dc521beab738e2
Philipp Fehre <philipp@fehre.co.uk>
2018-05-08 21:05:15 +0100

    start to write translation between api-gw and ring

--- src/playground/core.clj
+++ src/playground/core.clj
@@ -8,8 +8,28 @@
 
 (defn handle-event
   [event]
-  (println event)
   {:statusCode 200 :headers {} :body "Hello Lambda"})
+
+(defn event->request
+  [ev ctx]
+  {:server-port nil
+   :body nil
+   :server-name nil
+   :remote-addr nil
+   :uri nil
+   :query-string nil
+   :scheme nil
+   :request-method nil
+   :protocol nil
+   :headers nil
+   :event ev
+   :context ctx})
+
+(defn response->api-gw-response
+  [r]
+  {:statusCode (:status r)
+   :body (json/write-json (:body r))
+   :headers (:headers r)})
 
 (defn -handleRequest
   [_ in out ctx]
@@ -17,3 +37,8 @@
         res (handle-event ev)]
     (with-open [w (io/writer out)]
       (json/write res w))))
+
+
+
+;; TODO integrate this with ring, aka make handle-event a ring handler
+;; TODO create a ring adapter around api-gateway
